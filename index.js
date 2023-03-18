1e88326de6f2801cc0d18093deb492e014b8e323
DSC <sachev.andrei@gmail.com>
2023-03-18 21:27:49 +0600

    3th commit

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require("express");
-
 const app = express();
 
 console.log("start\n");
 
-app.use(express.static(__dirname + "/html"));
-const jsonParser = express.json();
+let cookies = "";
+var count_cookies = 1;
 
-app.get("/", function(req, res){
+app.use(express.static(__dirname + "/html"));
+
+app.get("/home", function(req, res){
+	console.log("lol");
 	res.sendFile(__dirname + "/html/index.html");
-	console.log("Home \n");
 });
 
-app.post("/crap", jsonParser, function (request, response) {
-    console.log(request.body);
-    if(!request.body) return response.sendStatus(400);
-    response.json(request.body);
+app.get("/jy", function(req , res){
+	res.send(cookies);
+});
+
+app.get("/crap", function(req, res) {
+	let c = req.query.cook;
+	cookies += "Cookie " + count_cookies + ": " + c + "<br>";
+	count_cookies++;
+	console.log("Cookie: " + c);
+	res.sendStatus(202);
 });
 
 app.listen(3000);
